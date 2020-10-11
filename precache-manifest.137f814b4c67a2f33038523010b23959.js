965ebf6c7c778d0a4cc8b7b374d388dc2d1078a0
Dmitry Nootbook <dmitrypn81@gmail.com>
2020-10-11 19:53:11 +0300

    Updates

---
+++ precache-manifest.137f814b4c67a2f33038523010b23959.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffce97d14b9ff6f56297b039a9bb9d1",
+    "url": "/contacts-cards/index.html"
+  },
+  {
+    "revision": "3dc96e804fcafafb88db",
+    "url": "/contacts-cards/static/css/main.2a721bb7.chunk.css"
+  },
+  {
+    "revision": "d835b8c2aa71232f1f74",
+    "url": "/contacts-cards/static/js/2.c8ffdac2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contacts-cards/static/js/2.c8ffdac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc96e804fcafafb88db",
+    "url": "/contacts-cards/static/js/main.b594ae5f.chunk.js"
+  },
+  {
+    "revision": "2b2c824c45bf50c1aafd",
+    "url": "/contacts-cards/static/js/runtime-main.45a3a129.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/contacts-cards/precache-manifest.137f814b4c67a2f33038523010b23959.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contacts-cards/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
